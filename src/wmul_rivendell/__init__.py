bc2c23fd695638c72d11d83746de5318ec05611e
Mike Stanley <MikeTheHammer@users.noreply.github.com>
2025-06-18 11:53:49 -0400

    Update pyproject.toml and version.

--- src/wmul_rivendell/__init__.py
+++ src/wmul_rivendell/__init__.py
@@ -24,5 +24,5 @@ You should have received a copy of the GNU General Public License along with
 wmul_rivendell. If not, see <https://www.gnu.org/licenses/>. 
 """
 
-__version__ = "0.13.1"
+__version__ = "0.14.0"
 
